129306579c2754ec6faa2d462b0fefde220a007d
Lucas Siqueira <lucassouza.ti@gmail.com>
2022-10-10 04:56:56 -0300

    feat: laravel sanctum authenticate by auth controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\LegalActController;
 
 
@@ -19,6 +20,9 @@ use App\Http\Controllers\Api\LegalActController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
 
 Route::apiResource('legalacts', LegalActController::class);
 
